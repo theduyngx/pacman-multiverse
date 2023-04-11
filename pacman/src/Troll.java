b45385a334456c4c916e8f3889b28c8e2dc0dd47
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-04-11 17:31:14 +1000

    Added a Troll Class

--- pacman/src/Troll.java
+++ pacman/src/Troll.java
@@ -2,13 +2,55 @@ package src;
 
 public class Troll extends Monster
 {
-    public Troll()
+    public Troll(Game game, String spriteName)
     {
-        super();
+        super(game, spriteName);
     }
 
     @Override
-    protected void walkApproach() {
+    protected void walkApproach()
+    {
+        double oldDirection = this.getDirection();
+        int sign = this.randomiser.nextDouble();
+        this.setDirection(oldDirection);
+        this.turn(sign*90);
 
+        next = getNextMoveLocation();
+
+        // First get a random direction to go to (left or right)
+        if (this.canMove(next))
+        {
+            this.setLocation(next);
+        }
+
+        // Collision occurs going first given driection
+        else
+        {
+            // Check if you can go the opposite turn,
+            // either left or right
+            this.setDirection(oldDirection);
+            this.turn(-sign*90);
+            next = this.getNextMoveLocation();
+
+            if (this.canMove(next))
+            {
+                this.setLocation(next);
+            }
+
+            // If nothing really worked, just go
+            // backwards
+            else
+            {
+                this.setDirection(oldDirection);
+                this.turn(180);
+                next = this.getNextMoveLocation();
+                this.setLocation(next);
+            }
+        }
+
+        // Tell game to change monster's location and store
+        // this as visited
+        game.getGameCallback().monsterLocationChanged(this);
+        this.addVisitedList(next);
     }
 }
