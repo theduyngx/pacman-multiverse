32739dc7712f7d31d246955ba9a100c61b7a4e95
theduynguyen <theduynguyen2001@gmail.com>
2023-04-12 00:24:55 +1000

    ObjectManager class init

--- pacman/src/ObjectManager.java
+++ pacman/src/ObjectManager.java
@@ -1,23 +1,29 @@
 package src;
 
 import ch.aplu.jgamegrid.Location;
-
 import java.util.ArrayList;
 import java.util.Properties;
 
 public class ObjectManager {
-    private final ArrayList<Monster> monsters;
     private final PacActor pacActor;
+    private final ArrayList<Monster> monsters;
     private final ArrayList<Pill> pills;
     private final ArrayList<Gold> golds;
     private final ArrayList<Ice> ices;
 
-    public PositionManager(PacActor pacActor) {
+    public ObjectManager(PacActor pacActor) {
+        if (pacActor == null) {
+            System.exit(1);
+        }
         this.monsters = new ArrayList<>();
         this.pacActor = pacActor;
         this.pills = new ArrayList<>();
         this.golds = new ArrayList<>();
         this.ices = new ArrayList<>();
+    }
+
+    public PacActor getPacActor() {
+        return pacActor;
     }
 
     public void parseProperties(Properties properties) {
@@ -30,15 +36,15 @@ public class ObjectManager {
             int posX = Integer.parseInt(pos[0]);
             int posY = Integer.parseInt(pos[1]);
 
-            Pill pill = Pill(new Location(posX, posY));
-            pillLocations.add(pill);
+            Pill pill = new Pill(new Location(posX, posY));
+            pills.add(pill);
         }
         for (String pL : pillLocations) {
             String[] pos = pL.split(",");
             int posX = Integer.parseInt(pos[0]);
             int posY = Integer.parseInt(pos[1]);
 
-            Pill pill = Pill(new Location(posX, posY));
+            Pill pill = new Pill(new Location(posX, posY));
         }
     }
 }
