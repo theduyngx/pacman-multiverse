3ff28d2381600114a007d6b6a559a2f080963ad8
theduynguyen <theduynguyen2001@gmail.com>
2023-04-20 15:03:15 +1000

    Movable: updated doco

--- pacman/src/utility/Movable.java
+++ pacman/src/utility/Movable.java
@@ -4,6 +4,11 @@ import src.Game;
 import ch.aplu.jgamegrid.Location;
 
 
+/**
+ * Movable interface representing any movable object within a game.
+ * @see Game
+ * @see src.LiveActor
+ */
 public interface Movable {
     /**
      * Interface method to put live actor to the game.
@@ -15,6 +20,7 @@ public interface Movable {
      * Interface method checking whether a movable game object can move to a specified location or not.
      * @param location the specified location
      * @return         boolean value indicating whether object can move to specified location
+     * @see            Location
      */
     boolean canMove(Location location);
 
@@ -27,6 +33,7 @@ public interface Movable {
      * Interface method add a specified location to the list of visited locations. Visited locations is
      * important in determining movable game object behaviors.
      * @param location specified visited location
+     * @see            Location
      */
     void addVisitedList(Location location);
 
@@ -35,6 +42,7 @@ public interface Movable {
      * since much of the movable game object behaviors is determined by their visited (and otherwise) locations.
      * @param location the specified location
      * @return         boolean indicating whether location has been visited within a cycle or not.
+     * @see            Location
      */
     boolean notVisited(Location location);
 }
